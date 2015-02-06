ce7ef96251552c971337e47c6064410d9cf0037a
camieac <camieac@gmail.com>
2015-02-06 17:58:13 +0000

    Can now move back to a square where the piece has been. (Bug Fix)

--- src/Square.java
+++ src/Square.java
@@ -57,6 +57,7 @@ public class Square extends JLabel implements MouseListener {
 	public void addPiece(ChessPiece chessPiece) {
 		if (chessPiece == null) {
 			this.setIcon(null);
+			this.piece = null;
 		} else {
 			piece = chessPiece;
 			this.setIcon(piece.getIcon());
